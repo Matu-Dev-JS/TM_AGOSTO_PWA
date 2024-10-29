5257a870bdb53405122bc0c09460306ea53e8237
matu.dev <matu.a.gimenez2002@gmail.com>
2024-10-29 11:58:07 -0300

    clase 25

---
+++ clase-21-22-23-24-25/src/repositories/product.repository.js
@@ -0,0 +1,27 @@
+import Product from "../models/product.model.js";
+
+class ProductRepository{
+    static async getProducts(){
+        //Obtener lista productos activos
+        return Product.find({active: true})
+    }
+
+    static async getProductById(id){
+        return Product.findById(id)
+    }
+
+    static async createProduct(product_data){
+        const new_product = new Product(product_data)
+        return new_product.save()
+    }
+
+    static async updateProduct(id, new_product_data){
+        return Product.findByIdAndUpdate(id, new_product_data, {new: true})
+    }
+
+    static  async deleteProduct(id){
+        return Product.findByIdAndUpdate(id, {active: false}, {new: true})
+    }
+}
+
+export default ProductRepository
